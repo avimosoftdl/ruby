6946d61e90b5c8e02ea3898b6c9b232d35d39546
Adrián Avilés <adrian.aviles@avimosoft.com>
2015-05-27 22:28:39 -0500

    archivos finales de la clase ruby_session03

--- saberinline.rb
+++ saberinline.rb
@@ -1,8 +1,8 @@
 require 'open-uri'
-web_contents  = open('http://www.en1mes.com') {|f| f.read }
+web_contents  = open('http://avimosoft.com/wp2013/') {|f| f.read }
 
 lines = web_contents.split
 lines.each_with_index do |p, i|
-  r = /style="(.*)"/.match p
-  puts "linea #{i} #{r.captures[0]}" if r
+  r = /<title>.*/.match p
+  puts "linea #{i} #{r}" if r
 end
